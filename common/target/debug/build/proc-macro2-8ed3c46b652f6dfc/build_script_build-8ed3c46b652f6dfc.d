a6e1d72a4cfa3e465d52a1d78fb4fee9b1417b14
JWZ1996 <46710795+JWZ1996@users.noreply.github.com>
2022-06-08 16:05:06 +0200

    Delete common/target directory

--- common/target/debug/build/proc-macro2-8ed3c46b652f6dfc/build_script_build-8ed3c46b652f6dfc.d
+++
@@ -1,5 +0,0 @@
-/home/kacper31/Studia/II_rok/JNP2-Rust/project-chatter/common/target/debug/build/proc-macro2-8ed3c46b652f6dfc/build_script_build-8ed3c46b652f6dfc: /home/kacper31/.cargo/registry/src/github.com-1ecc6299db9ec823/proc-macro2-1.0.38/build.rs
-
-/home/kacper31/Studia/II_rok/JNP2-Rust/project-chatter/common/target/debug/build/proc-macro2-8ed3c46b652f6dfc/build_script_build-8ed3c46b652f6dfc.d: /home/kacper31/.cargo/registry/src/github.com-1ecc6299db9ec823/proc-macro2-1.0.38/build.rs
-
-/home/kacper31/.cargo/registry/src/github.com-1ecc6299db9ec823/proc-macro2-1.0.38/build.rs:
--- common/target/debug/deps/unicode_xid-5409942a6294a17e.d
+++
@@ -1,8 +0,0 @@
-/home/kacper31/Studia/II_rok/JNP2-Rust/project-chatter/common/target/debug/deps/unicode_xid-5409942a6294a17e.rmeta: /home/kacper31/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-xid-0.2.3/src/lib.rs /home/kacper31/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-xid-0.2.3/src/tables.rs
-
-/home/kacper31/Studia/II_rok/JNP2-Rust/project-chatter/common/target/debug/deps/libunicode_xid-5409942a6294a17e.rlib: /home/kacper31/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-xid-0.2.3/src/lib.rs /home/kacper31/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-xid-0.2.3/src/tables.rs
-
-/home/kacper31/Studia/II_rok/JNP2-Rust/project-chatter/common/target/debug/deps/unicode_xid-5409942a6294a17e.d: /home/kacper31/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-xid-0.2.3/src/lib.rs /home/kacper31/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-xid-0.2.3/src/tables.rs
-
-/home/kacper31/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-xid-0.2.3/src/lib.rs:
-/home/kacper31/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-xid-0.2.3/src/tables.rs:
